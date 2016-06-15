0d7e9d1c93c01869230d77ba1e746db13bcaac5b
Jia Huang <iamfat@gmail.com>
2016-06-15 19:06:06 +0800

    enhancement

--- 5-door-control/HTTP.php
+++ 5-door-control/HTTP.php
@@ -4,19 +4,22 @@ $server = new swoole_http_server("0.0.0.0", 80);
 
 $server->on('request', function ($request, $response) {
     $uri = $request->server['request_uri']; 
-    echo $request->server['remote_addr'].'> '.$request->server['request_method'].' '.$uri."\n";
+   if (!$uri || $uri=='/') {
+        $uri = '/index.html';
+    }
+
+    printf("%s: %s \e[32m%s\e[0m\n",
+        $request->server['remote_addr'], $request->server['request_method'],
+        $uri);
+ 
     $root = rtrim(__DIR__, '/').'/web';
     $file = $root . '/'. ltrim($uri, '/'); 
     if (file_exists($file) && is_file($file)) { 
         $mime = mime_content_type($file);
         $response->header('Content-Type', $mime); 
         $response->sendfile($file); 
-        return; 
-    } 
-
-    if (!$uri || $uri='/') {
-        $content = file_get_contents($root.'/index.html');
-        $response->end($content);
+    } else { 
+        $response->end('');
     }
 });
 
--- 5-door-control/WebSocket.php
+++ 5-door-control/WebSocket.php
@@ -6,34 +6,26 @@ $hardware = new Hardware('/dev/ttyO1');
 $server = new swoole_websocket_server("0.0.0.0", 9501);
 
 $server->on('open', function (swoole_websocket_server $server, $request) {
-    echo "有人连接 (fd=$request->fd)\n";
+    printf("%s: 连接WebSocket (fd=%s)\n", $request->server['remote_addr'], $request->fd);
 });
 
 $server->on('message', function (swoole_websocket_server $server, $frame) 
     use ($hardware) {
         $d = (object) @json_decode($frame->data, true);
-        echo "收到 $d->command\n";
         if ($d->command == 'open') {
-            echo "尝试开门!\n";
+            echo "[WebSocket] 尝试开门...\n";
             $hardware->open();
         } elseif ($d->command == 'card') {
-            $message = json_encode([
-                'event' => 'card',
-                'data' => [
-                    'card_no' => '123456'
-                ]
-            ]);
-            foreach($server->connections as $fd) {
-                $server->push($fd, $message);
-            }
+            echo "[WebSocket] 模拟刷卡...\n";
+            $hardware->emit('card', '123456');
         }
 });
 
 $server->on('start', function (swoole_websocket_server $server) use ($hardware) {
-    echo "WebSocket服务器开启\n";
+    echo "[WebSocket] 服务器开启\n";
 
     $hardware->on('card', function($cardNumber) use ($server) {
-        printf ("获取到卡号 %s, 发送给所有人\n", $cardNumber);
+        printf ("[Hardware] 获取到卡号 (%s), 通知 WebSocket\n", $cardNumber);
         $message = json_encode([
             'event' => 'card',
             'data' => [
